37042e688de6fb1a04c56ea0bbb28d2b2fab8d6b
whiskers75 <crazycats@yopmail.com>
2012-08-20 03:32:48 -0400

    Removed Identify

--- irc.js
+++ irc.js
@@ -4,7 +4,6 @@ var request = require('request');
 var xml2js = require('xml2js');
 var Bitly = require('bitly');
 var bitly = new Bitly('freenode', 'R_d143d45888039a84c912c6f057c11326');
-var userpass = "none";
 
 
 /*
@@ -105,8 +104,6 @@ var botSlave = new irc.Client('irc.freenode.net', 'IRCbot_Slave', {
   floodProtectionDelay: 1000,
   stripColors: false
 });
-botMaster.send('MSG NickServ IDENTIFY ' + userpass);
-botSlave.send('MSG NickServ IDENTIFY ' + userpass);
 botMaster.addListener('registered', function(message) {
     console.log('Connected!');
     console.log(message);
