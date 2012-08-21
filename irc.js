62db056c33adfa4e62c14d03f31d8352fb1a4bc1
whiskers75 <crazycats@yopmail.com>
2012-08-21 05:15:52 -0400

    Added Eval

--- irc.js
+++ irc.js
@@ -193,7 +193,7 @@ botMaster.addListener('pm', function(sender, message) {
     botMaster.say(currentChannel, sender  + ": disabling IRCbot...");
 	init = false;
   }
-  if(startsWith(message, 'eval')) {
+  if(startsWith(message, 'eval ')) {
       var expr = message.split(' ').splice(1).join(' ');
       try {
         eval(expr);
